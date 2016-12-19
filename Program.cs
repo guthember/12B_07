1583091c17e4abcd258b067afd703eb9b81147e4
Erik Gúth <guthember@gmail.com>
2016-12-19 10:13:52 +0100

    Elso

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SzamokTomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[5];
+
+            Console.WriteLine("Kérek 5 darab számot.");
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write("{0}. szam:",i+1);
+                tomb[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
